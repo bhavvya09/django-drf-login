462669db4e008f2694b9366faa9c6cf5da156592
Bhavya singh <112320195+bhavvya09@users.noreply.github.com>
2024-03-28 10:07:42 +0530

    Add files via upload

---
+++ django-drf-login/src/first/views.py
@@ -0,0 +1,17 @@
+
+
+# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
+
+from .serializers import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    UserModel View.
+    """
+
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
